8b1b019e24f3e44a0f203bdb9188f284c07d082b
DRN-DevLarx <drndevlarx@gmail.com>
2026-01-08 18:43:27 -0600

    Arreglo de espaciado

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 function About() {
     return (
         <section class="bg-[#adb6aaa8] dark:bg-[#171731]">
-            <div class="container px-6 py-10 mx-auto">
+            <div class="container px-6 py-5 mx-auto">
                 <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white"> Sobre Nosotros</h1>
 
-                <div class="mt-2">
+                <div>
                     <span class="inline-block w-40 h-1 bg-[#38664e] rounded-full"></span>
                     <span class="inline-block w-3 h-1 ml-1 bg-[#38664e] rounded-full"></span>
                     <span class="inline-block w-1 h-1 ml-1 bg-[#38664e] rounded-full"></span>
                 </div>
 
-                <div class="mt-8 xl:mt-12 lg:flex lg:items-center">
+                <div class="mt-2 lg:flex lg:items-center">
                     <div class="grid w-full grid-cols-2 gap-8 lg:w-1/2 xl:gap-16 md:grid-cols-2">
 
                         <div class="space-y-3 col-span-2">
@@ -40,7 +40,7 @@ function About() {
                     </div>
 
                     <div class="hidden lg:flex lg:w-1/2 lg:justify-center">
-                        <img class="w-[28rem] h-[28rem] flex-shrink-0 object-cover xl:w-[34rem] xl:h-[34rem] rounded-full" src="https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
+                        <img class="w-[25rem] h-[25rem] flex-shrink-0 object-cover rounded-full" src="https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
                     </div>
                 </div>
             </div>
