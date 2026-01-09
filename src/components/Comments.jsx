91cc3b28fb130a49fbd3b14f8665f0a8617967ec
DRN-DevLarx <drndevlarx@gmail.com>
2026-01-08 18:51:13 -0600

    Cambio comentarios (Datos quemados)

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,41 +1,125 @@
-import React from 'react'
-import UserImage from "../assets/UserImage.png";
+import { useState, useEffect } from "react";
+import { GetData } from "../services/ApiServices";
 
-function Comments() {
-    return (
-        <section className="bg-[#adb6aaa8] dark:bg-[#171731]  px-4">
-            <hr className='text-gray-500 opacity-70'/>
-            
-            <div className="py-10 max-w-6xl mx-auto grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
-                
-                <article className="bg-[#adb6aa] dark:bg-gray-800 p-6 rounded-lg shadow-md">
-                    <div className="flex items-center mb-4">
-                        <img className="w-10 h-10 me-4 rounded-full" src={UserImage} alt=""/>
-                        <div className="font-medium dark:text-white">
-                            <p>María González <time datetime="2020-06-10 19:00" className="block text-sm text-gray-500 dark:text-gray-400">Joined on June 2020</time></p>
-                        </div>
-                    </div>
-                    <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
-                        {/* Estrellas */}
-                        <svg className="w-4 h-4 text-yellow-300" fill="currentColor" viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg"><path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/></svg>
-                        <svg className="w-4 h-4 text-yellow-300" fill="currentColor" viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg"><path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/></svg>
-                        <svg className="w-4 h-4 text-yellow-300" fill="currentColor" viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg"><path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/></svg>
-                        <svg className="w-4 h-4 text-yellow-300" fill="currentColor" viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg"><path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/></svg>
-                        <svg className="w-4 h-4 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg"><path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/></svg>
-                        <h3 className="ms-2 text-sm font-semibold text-gray-900 dark:text-white">Excelente calidad y servicio</h3>
-                    </div>
-                    <footer className="mb-5 text-sm text-gray-500 dark:text-gray-400">
-                    </footer>
-                    <p className="mb-2 text-gray-500 dark:text-gray-400">
-                        He comprado varios productos de este abastecedor y la calidad siempre ha sido sobresaliente. Cada entrega llega a tiempo y en perfecto estado.
-                        Además, sus precios son muy competitivos y el servicio al cliente responde rápidamente a cualquier duda. Sin duda seguiré comprando aquí.
-                    </p>
-                
-                </article>
+const comments = () => {
 
+    const [UserImage, setUserImage] = useState()
+    const [UserNames, setUserNames] = useState("")
+    const [UserLasNames, setUserLasNames] = useState("")
+    const [UserComment, setUserComment] = useState("")
+    const [CommentDate, setCommentDate] = useState()
+    
+    useEffect(() => {
+      const fetch = async () => {
+          const UsersData = await GetData("users/")
+    
+          if (UsersData) {
+              console.log(UsersData);
+              
+          }
+      }
+    
+      fetch();
+    
+    }, []);
+
+
+    const cardsData = [
+        {
+            image: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200',
+            name: 'Briar Martin',
+            handle: '@neilstellar',
+            message: 'Radiant made undercutting all of our competitors an absolute breeze.',
+            date: 'April 20, 2025'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200',
+            name: 'Avery Johnson',
+            handle: '@averywrites',
+            message: 'Radiant made undercutting all of our competitors an absolute breeze.',
+            date: 'May 10, 2025'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=200&auto=format&fit=crop&q=60',
+            name: 'Jordan Lee',
+            handle: '@jordantalks',
+            message: 'Radiant made undercutting all of our competitors an absolute breeze.',
+            date: 'June 5, 2025'
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=200&auto=format&fit=crop&q=60',
+            name: 'Avery Johnson',
+            handle: '@averywrites',
+            message: 'Radiant made undercutting all of our competitors an absolute breeze.',
+            date: 'May 10, 2025'
+        },
+    ];
+
+    const CreateCard = ({ card }) => (
+        <div className="p-4 rounded-lg mx-4 border border-gray-500 shadow hover:shadow-lg transition-all duration-200 w-72 shrink-0">
+            <div className="flex items-center gap-2">
+                <img className="size-11 rounded-full" src={card.image} alt="User Image" />
+                <div className="flex flex-col">
+                    <div className="flex items-center gap-1">
+                        <p>{card.name}</p>
+                    </div>
+                </div>
             </div>
-        </section>
+            <p className="text-sm py-4 text-gray-500"> {card.message} </p>
+            <div className="flex items-center justify-rigth text-slate-500 text-xs">
+                <p>{card.date}</p>
+            </div>
+        </div>
+    );
+
+    return (
+        <>
+            <style>{`
+            @keyframes marqueeScroll {
+                0% { transform: translateX(0%); }
+                100% { transform: translateX(-50%); }
+            }
+
+            .marquee-inner {
+                animation: marqueeScroll 30s linear infinite;
+            }
+
+            .marquee-reverse {
+                animation-direction: reverse;
+            }
+        `}</style>
+
+                
+                <div className="bg-[#adb6aaa8] dark:bg-[#171731] pl-30 pt-20">
+                    <h1 class="text-2xl font-semibold text-gray-800  lg:text-3xl dark:text-white"> ¿Que dicen nuestros clientes?</h1>
+                    <div>
+                        <span class="inline-block w-40 h-1 bg-[#38664e] rounded-full"></span>
+                        <span class="inline-block w-3 h-1 ml-1 bg-[#38664e] rounded-full"></span>
+                        <span class="inline-block w-1 h-1 ml-1 bg-[#38664e] rounded-full"></span>
+                    </div>
+                </div> 
+            <div className="bg-[#adb6aaa8] dark:bg-[#171731] marquee-row w-full mx-auto overflow-hidden relative">
+
+                <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-gray-300 dark:from-gray-700 to-transparent"></div>
+                <div className="marquee-inner flex transform-gpu min-w-[200%] pt-5 pb-1">
+                    {[...cardsData, ...cardsData].map((card, index) => (
+                        <CreateCard key={index} card={card} />
+                    ))}
+                </div>
+                <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-gray-300 dark:from-gray-700 to-transparent"></div>
+            </div>
+
+            <div className="bg-[#adb6aaa8] dark:bg-[#171731] marquee-row w-full mx-auto overflow-hidden relative">
+                <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-gray-300 dark:from-gray-700 to-transparent"></div>
+                <div className="marquee-inner marquee-reverse flex transform-gpu min-w-[200%] pt-10 pb-10">
+                    {[...cardsData, ...cardsData].map((card, index) => (
+                        <CreateCard key={index} card={card} />
+                    ))}
+                </div>
+                <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-gray-300 dark:from-gray-700 to-transparent"></div>
+            </div>
+        </>
     )
 }
 
-export default Comments
+export default comments
