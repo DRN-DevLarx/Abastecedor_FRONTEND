d0405adb1f51a21050fb21806020a28b549d138f
DRN-DevLarx <drndevlarx@gmail.com>
2026-01-17 02:40:22 -0600

    Agrego la funcion para detectar el tema del usuario y cambiarlo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,63 @@
+import { useState, useEffect } from "react";
+import { getCookie } from "./services/Token/sessionManager";
+import { GetData, PatchData } from "./services/ApiServices";
 import Routing from "./routes/Routing";
-import { ThemeProvider } from "../src/components/ThemeContext.jsx";
+import { AutenticatedUserData } from "./services/Token/AuthServices";
+import { ThemeProvider } from "../src/components/ThemeContext";
 
 function App() {
+  const access_token = getCookie("access_token");
+  let userQuery = null;
+
+  if (access_token) {
+    userQuery = AutenticatedUserData();
+  }
+
+  const [darkMode, setDarkMode] = useState(false);
+  const [notificationsOn, setNotificationsOn] = useState(false);
+
+  
+  // 2) Escucha cambios de darkMode y aplica la clase
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  // 3) Alternar manualmente en vivo
+  const toggleTheme = async () => {
+    const newTheme = darkMode ? "normal" : "oscuro";
+
+    // ⚡ Cambiar primero el estado para reflejarlo en UI inmediatamente
+    setDarkMode(!darkMode);
+
+    // ⏳ Luego guardar en la BD
+    try {
+      const PatchTheme = await PatchData("informacionUsuarios/", userQuery.data.id, {
+        tema: newTheme
+      })
+
+    } catch (error) {
+      console.error("Error al guardar tema en BD:", error);
+    }
+  };
+
+
   return (
     <ThemeProvider>
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
+        {/* Botón de toggle */}
+        <button
+          onClick={toggleTheme}
+          className="z-100 fixed top-80 right-30 px-4 py-2 rounded dark:bg-red-600 bg-blue-500 text-white transition"
+        >
+          {darkMode ? "Modo Claro" : "Modo Oscuro"}
+        </button>
+
         <Routing />
+      </div>
     </ThemeProvider>
   );
 }
